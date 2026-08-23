23995653a9a41af017f64193cd64de3698b82540
sdzahag4 <sdzahag4@gmail.com>
2026-08-23 08:09:19 +0700

    Bersihkan root build.gradle.kts dari blok subprojects

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -8,19 +8,3 @@ allprojects {
 tasks.register<Delete>("clean") {
     delete(rootProject.layout.buildDirectory)
 }
-
-subprojects {
-    afterEvaluate { proj: org.gradle.api.Project ->
-        if (proj.hasProperty("android")) {
-            val androidExtension = proj.extensions.findByName("android")
-            if (androidExtension != null) {
-                try {
-                    val method = androidExtension.javaClass.getMethod("compileSdkVersion", Int::class.java)
-                    method.invoke(androidExtension, 34)
-                } catch (e: Exception) {
-                    // Abaikan jika tidak mendukung
-                }
-            }
-        }
-    }
-}
