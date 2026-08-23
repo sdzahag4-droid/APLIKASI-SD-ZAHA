54973fbd722265ef663dae959658e12f2fb4dbe7
sdzahag4 <sdzahag4@gmail.com>
2026-08-23 08:04:30 +0700

    Fix lStar error with subprojects compileSdkVersion in build.gradle.kts and update printing version

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -8,3 +8,13 @@ allprojects {
 tasks.register<Delete>("clean") {
     delete(rootProject.layout.buildDirectory)
 }
+
+subprojects {
+    afterEvaluate { project ->
+        if (project.plugins.hasPlugin("com.android.application") || project.plugins.hasPlugin("com.android.library")) {
+            project.extensions.configure<com.android.build.gradle.BaseExtension> {
+                compileSdkVersion(34)
+            }
+        }
+    }
+}
