45b94b0eec2fa3c397909e4fe19131331ba98676
sdzahag4 <sdzahag4@gmail.com>
2026-08-23 09:31:51 +0700

    fix: update compileSdk to 36 to match AndroidX library requirements

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -5,6 +5,21 @@ allprojects {
     }
 }
 
+val newBuildDir: Directory = rootProject.layout.buildDirectory.dir("../../build").get()
+rootProject.layout.buildDirectory.value(newBuildDir)
+
+subprojects {
+    val subproject = this
+    subproject.afterEvaluate {
+        if (subproject.plugins.hasPlugin("com.android.application") || subproject.plugins.hasPlugin("com.android.library")) {
+            subproject.extensions.configure<com.android.build.gradle.BaseExtension> {
+                compileSdkVersion(36)
+                buildToolsVersion("36.0.0")
+            }
+        }
+    }
+}
+
 tasks.register<Delete>("clean") {
     delete(rootProject.layout.buildDirectory)
 }
