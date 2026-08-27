afd04c38729851f7a9d71b6d9fee5947d946e60e
sdzahag4 <sdzahag4@gmail.com>
2026-08-28 02:37:11 +0700

    Fix import config di absen mapel screen

--- lib/screens/absen_mapel_screen.dart
+++ lib/screens/absen_mapel_screen.dart
@@ -1,7 +1,7 @@
 import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
-import '../config.dart';
+import 'package:aplikasi_sd_zaha/config.dart';
 
 class AbsenMapelScreen extends StatefulWidget {
   final String namaGuru;
